1b9786da05309d6d78e896b05ae03e3321c8fb64
Lucas <lucascostamonteiro@yahoo.com>
2023-12-11 13:07:49 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import HomePage from './components/Home'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <HomePage />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Home/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const HomePage = () => {
+
+
+  return (
+    <div>
+      <h1>
+  Wauriman Borges BJJ
+        {/* Other head elements */}
+      </h1>
+
+        <section id="header">
+          {/* Header content */}
+        </section>
+
+        <section id="one" className="main style1">
+          {/* Section One content */}
+        </section>
+
+        <section id="two" className="main style1 special">
+          {/* Section Two content */}
+        </section>
+
+        <section id="three">
+          {/* Footer content */}
+        </section>
+
+    </div>
+  );
+};
+
+export default HomePage;
